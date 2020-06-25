cb172fc276581ab7042ebba4d695ca9fba4d99c6
Adam Emieljaniuk <beginner1986@gmail.com>
2020-06-25 18:27:52 +0200

    Parser methods implemented. getCoinFromSingleRow need improvement.

--- CoinsCatalogDataDownloader/src/main/java/Main.java
+++ CoinsCatalogDataDownloader/src/main/java/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Parser parser = new Parser();
+		Parser parser = Parser.getInstance();
 		
 		parser.parse();
 	}
--- CoinsCatalogDataDownloader/src/main/java/Parser.java
+++ CoinsCatalogDataDownloader/src/main/java/Parser.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -8,15 +9,22 @@ import org.jsoup.select.Elements;
 
 public class Parser {
 
+	private static Parser instance = null;
 	private final String urlTemplate = "https://katalogfischer.pl/?from_1=";
+
+	private Parser() {}
 	
-	public Parser() {
-		// TODO: singleton
+	public static Parser getInstance() {
+		if(instance == null)
+			instance = new Parser();
+		
+		return instance;
 	}
 	
 	public List<Coin> parse() {
 
-		int initNumber = 0;		// it be iterated by 20 in final version
+		int initNumber = 0;		// it will be iterated by 20 in final version
+		
 		Document document = openCatalogPage(initNumber);
 		Element table = getTable(document);
 		Elements tableRows = getTableRows(table);
@@ -47,24 +55,54 @@ public class Parser {
 	
 	// the first table contains coins data
 	private Element getTable(Document document) {
-		// TODO 
-		return null;
+		Elements tables = document.getElementsByTag("table");
+		Element table = tables.first();
+		
+		return table;
 	}
 	
 	// each table row contains single coin data
 	private Elements getTableRows(Element table) {
-		// TODO
-		return null;
+		Elements tableRows = table.getElementsByTag("tr");
+
+		return tableRows;
 	}
 	
 	private List<Coin> getCoinsFromTableRows(Elements tableRows) {
-		// TODO
-		return null;
+		List<Coin> coins = new ArrayList<Coin>();
+		
+		for(int i=1; i<tableRows.size(); i+=3) {
+			Coin coin = getCoinFromSingleRow(tableRows.get(i));
+			coins.add(coin);
+		}
+		
+		return coins;
 	}
 	
 	private Coin getCoinFromSingleRow(Element tableRow) {
-		// TODO
-		return null;
+		Coin coin = new Coin();
+		List<String> attributes = new ArrayList<String>();
+		
+		Elements tds = tableRow.getElementsByTag("td");
+		Elements imgs = tableRow.getElementsByTag("img");
+		
+		for(Element td : tds) {
+			Elements strongs = td.getElementsByTag("strong");
+			for(Element strong : strongs) {
+				attributes.add(strong.text());
+			}
+		}
+		
+		attributes.add(imgs.get(0).attr("src"));
+		
+		// DEBUG
+		for(String s : attributes)
+			System.out.print(s + " | ");
+		System.out.println();
+		
+		// TODO: should read empty fields
+		
+		return coin;
 	}
 	
 }
